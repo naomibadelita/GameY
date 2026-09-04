35ff0a95544f1f0ac891421ac6b3ba97e3d7a230
Bogdan <bogdanzaharia904@yahoo.com>
2026-09-04 15:53:11 +0300

    feat: avatar selection

--- webapp/src/statistics/data/profile.loader.ts
+++ webapp/src/statistics/data/profile.loader.ts
@@ -1,6 +1,10 @@
-import { loadProfile as loadProfileHttp } from "../../api";
+import { loadProfile as loadProfileHttp, setProfilePhotoUrl as setProfilePhotoUrlHttp } from "../../api";
 import type { Profile } from "../domain/profile.entity";
 
 export function loadProfile(uid: string): Promise<Profile> {
     return loadProfileHttp(uid);
+}
+
+export function setPhotoUrl(photoUrl: string | null): Promise<Profile> {
+    return setProfilePhotoUrlHttp(photoUrl);
 }
